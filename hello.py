e3a645c80f7706786834d4b71db36a9e08badcc3
diman-dn <dmitri_mityashin@mail.ru>
2018-03-04 21:53:33 +0300

    1

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
-def application(environ, start_response):
-	
+from cgi import parse_qs
+
+def app(environ, start_response):
+	query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
+	body = []
+	for key, value in query.items():
+		for item in value:
+			body.append(key + "=" + item + "\r\n")
+
 	status = '200 OK'
 	headers = [
-		('Content-Type', 'text-plain')
+		('Content-type', 'text-plain')
 	]
-	body = ''
 	start_response(status, headers)
-	return [body]
+	return body
--- init.sh
+++ init.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/web
+sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
