7d0ebb6db5d56945fd2bb217fc95c5241d1ba50c
diman-dn <dmitri_mityashin@mail.ru>
2018-03-08 14:45:41 +0200

    1

--- init.sh
+++ init.sh
@@ -4,12 +4,12 @@ sudo rm /etc/gunicorn.d/ask
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
+
 #sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/web
 #sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
-sudo gunicorn -c /home/box/web/etc/gunicorn_ask.conf ask.wsgi:application
-sudo /etc/init.d/gunicorn restart
-
-#sudo gunicorn -b 0.0.0.0:8080 hello:app
+sudo gunicorn -D -b 0.0.0.0:8080 -c /home/box/web/etc/gunicorn.conf hello:app
+cd ask/
+sudo gunicorn -D -b 0.0.0.0:8000 -c /home/box/web/etc/gunicorn_ask.conf ask.wsgi:application
+#sudo /etc/init.d/gunicorn restart
 
 #sudo /etc/init.d/mysql start
